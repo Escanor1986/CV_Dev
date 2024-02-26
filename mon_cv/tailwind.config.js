464bb7c68e2c02edac7b945f09490c8fa56e55a8
Escanor1986 <lionel.zovi@gmail.com>
2024-02-26 16:26:17 +0100

    content and beginning style

--- mon_cv/tailwind.config.js
+++ mon_cv/tailwind.config.js
@@ -1,9 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'synth-pink': '#ff007a',
+        'synth-blue': '#00d9e9',
+        'synth-purple': '#bd00ff',
+        'nature-green': '#3f704d',
+      },
+    },
   },
   plugins: [],
-}
-
+};
